e428e08026f1702d939898f6931812bc71217cb2
KMDS <denkolobusik@gmail.com>
2026-04-21 15:40:11 +0300

    Lesson work

---
+++ quicksort.py
@@ -0,0 +1,10 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        less = [i for i in arr[1:] if i < arr[0]]
+        more = [i for i in arr[1:] if i >= arr[0]]
+        return quicksort(less) + [arr[0]] + quicksort(more)
+
+a = list(map(int, input().split()))
+print(quicksort)
